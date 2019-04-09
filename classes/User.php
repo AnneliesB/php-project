82b743e9d3a19f66ceb514b892b4cb30c24336cd
Annelies Bellon <annelies_bellon@hotmail.com>
2019-04-09 11:25:19 +0200

    create private variables with getters and setters
    All of the variables belonging to users that can be found in the database have been created and for each variable, getters ans setters have been generated. I also created an empty register function to prepare the register feature

---
+++ classes/User.php
@@ -0,0 +1,151 @@
+<?php
+
+class User{
+    private $username;
+    private $email;
+    private $password;
+    private $passwordConfirmation;
+    private $firstname;
+    private $lastname;
+    private $image;
+    private $description;
+
+
+    /**
+     * @return username
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @param $username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * @return email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return passwordConfirmation
+     */
+    public function getPasswordConfirmation()
+    {
+        return $this->passwordConfirmation;
+    }
+
+    /**
+     * @param $passwordConfirmation
+     */
+    public function setPasswordConfirmation($passwordConfirmation)
+    {
+        $this->passwordConfirmation = $passwordConfirmation;
+    }
+
+    /**
+     * @return firstname
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param $firstname
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return lastname
+     */
+    public function getLastname()
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param $lastname
+     */
+    public function setLastname($lastname)
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return image
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+    /**
+     * @return description (bio)
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param $description
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return boolean - true if successful, false if unsuccessful
+     */
+    public function register(){
+
+    }
+
+
+
+}
