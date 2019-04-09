e89f2e65271fc407e659a21609d9590454e0bfba
Annelies Bellon <annelies_bellon@hotmail.com>
2019-04-09 15:22:55 +0200

    add login html form code
    This code inclused the form with an email and password field and a submit button. In case there is no account yet, a link is provided to reroute. A form error section has been added too and will only be displayed if the  variable is set to true.

---
+++ login.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>IMDSTAGRAM - login</title>
+</head>
+<body>
+
+<div class="login">
+    <div class="form formLogin">
+        <form action="" method="post">
+            <h2 class="formTitle">Sign In</h2>
+
+            <?php if (isset($error)): ?>
+                <div class="formError">
+                    <p>
+                        Sorry, we can't log you in with that email address and password. Can you try again?
+                    </p>
+                </div>
+            <?php endif; ?>
+
+            <div class="formField">
+                <label for="email">Email</label>
+                <input type="text" name="email">
+            </div>
+            <div class="formField">
+                <label for="password">Password</label>
+                <input type="password" name="password">
+            </div>
+            <div class="formField">
+                <input type="submit" value="Sign In" class="btn btnPrimary">
+                <input type="checkbox" id="rememberMe"><label for="rememberMe" class="labelInline">Remember me</label>
+            </div>
+
+            <div>
+                <p>No account yet? <a href="register.php"> Sign up here</a></p>
+            </div>
+        </form>
+    </div>
+</div>
+
+</body>
+</html>
