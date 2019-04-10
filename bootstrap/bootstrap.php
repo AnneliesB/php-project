2baf92a0d17233b8e2ea922340e61b71fc42629f
Annelies Bellon <annelies_bellon@hotmail.com>
2019-04-10 11:58:53 +0200

    fix the bootstrap path and start a session
    The path was wrong due to a different folder structure. The autoloader also required a session to be started in order to work

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -1,9 +1,7 @@
 <?php
-
-/**
- * volgens mij moet hier een sessie lopende zijn, maar ik ben niet zeker
- */
-
+//allow sessions to be used everywhere bootstrap is required
+session_start();
+//autoload classes when requiring this bootstrap file
 spl_autoload_register(function($class){
-   require_once (__DIR__.DIRECTORY_SEPARATOR."classes".DIRECTORY_SEPARATOR.$class."php");
+    require_once(__DIR__ . "/.." . DIRECTORY_SEPARATOR . "classes" . DIRECTORY_SEPARATOR . $class . ".php");
 });
