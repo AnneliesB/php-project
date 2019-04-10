3bcbcbdc5942d09c980920f7e65271387d82d642
Annelies Bellon <annelies_bellon@hotmail.com>
2019-04-10 12:53:55 +0200

    change config file path and check spelling

--- classes/Db.php
+++ classes/Db.php
@@ -1,5 +1,5 @@
 <?php
-//Abstract class means we cannot get a new instance (object) of this class HOWEVER we van adress it to get a connection established
+//Abstract class means we cannot get a new instance (object) of this class HOWEVER we vcn address it to get a connection established
 abstract class Db{
     private static $conn; //Using static since we don't get any instances
 
@@ -9,7 +9,7 @@ abstract class Db{
             return self::$conn;
         }else{
             //no connection found, establisch a new one with config.ini file
-            $config = parse_ini_file("config/config.ini");
+            $config = parse_ini_file("config.ini");
             self::$conn = new PDO("mysql:host=" . $config['host'] . ";dbname=" . $config['db_name'], $config['db_user'], $config['db_password']);
             return self::$conn;
         }
