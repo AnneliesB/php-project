d75fb6dada74687d17dd10884a1eeab2f13015e2
Vince van alphen <vincevanalphen@yahoo.com>
2019-04-12 22:14:10 +0200

    updated password change
    Optimised password change

--- profile.php
+++ profile.php
@@ -1,41 +1,31 @@
 <?php
+session_start();
+include 'Db.php';
 
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "image/png"))
-&& ($_FILES["file"]["size"] < 1048600)) // less than 1MB
-  {
-  if ($_FILES["file"]["error"] > 0)
-    {
-    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
-    }
-  else
-    {
-    header("Location: step4.php");
-/*
-    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
-    echo "Type: " . $_FILES["file"]["type"] . "<br />";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
-    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
-
-*/
-    if (file_exists("upload/" . $_FILES["file"]["name"]))
-      {
-      echo $_FILES["file"]["name"] . " already exists. ";
-      }
-    else
-      {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "upload/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-      }
-    }
-  }
-else
-  {
-  echo "Invalid file";
-  }
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $newpassword = $_POST['newpassword'];
+        $confirmnewpassword = $_POST['confirmnewpassword'];
+        $result = mysql_query("SELECT password FROM user_info WHERE 
+user_id='$username'");
+        if(!$result)
+        {
+        echo "The username you entered does not exist";
+        }
+        else if($password!= mysql_result($result, 0))
+        {
+        echo "You entered an incorrect password";
+        }
+        if($newpassword=$confirmnewpassword)
+        $sql=mysql_query("UPDATE user_info SET password='$newpassword' where user_id='$username'");
+        if($sql)
+        {
+        echo "Congrats, you have a new password";
+        }
+       else
+        {
+       echo "Passwords do not match";
+       }
 ?>
 
 <!DOCTYPE html>
@@ -49,22 +39,34 @@ else
 <body>
 
 <form action="upload.php" method="post" enctype="multipart/form-data">
-<p>
-<label for="file0">Filename: </label>
-<input name="file[]" type="file" id="file0" size="20" />
-</p>
-<p>
-<label for="file0desc">Description: </label>
-<textarea rows="10" cols="30" id="file0desc" class="textarea"></textarea>
-</p>
-<p>
-<label for="file1desc">Description: </label>
-<textarea rows="10" cols="30" id="file1desc" class="textarea"></textarea>
-</p>
-<p>
-<input id="submit" type="submit" name="submit" value="Continue to Step 4" />
-</p>
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
 </form>
 
+<h1>Change Password</h1>
+   <form method="POST" action="password_change.php">
+    <table>
+    <tr>
+   <td>Enter your UserName</td>
+    <td><input type="username" size="10" name="username"></td>
+    </tr>
+    <tr>
+    <td>Enter your existing password:</td>
+    <td><input type="password" size="10" name="password"></td>
+    </tr>
+  <tr>
+    <td>Enter your new password:</td>
+    <td><input type="password" size="10" name="newpassword"></td>
+    </tr>
+    <tr>
+   <td>Re-enter your new password:</td>
+   <td><input type="password" size="10" name="confirmnewpassword"></td>
+    </tr>
+    </table>
+    <p><input type="submit" value="Update Password">
+    </form>
+   <p><a href="home.php">Home</a>
+   <p><a href="logout.php">Logout</a>
 </body>
 </html>
