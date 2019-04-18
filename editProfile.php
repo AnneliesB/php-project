3e3139843933df0be22540c9873f1c389433ed3b
Annelies Bellon <annelies_bellon@hotmail.com>
2019-04-18 15:45:41 +0200

    add password check for editProfile page
    To prevent accounts getting stolen based on session cookies, ... the profile data can only be edited after filling in the user's password

--- editProfile.php
+++ editProfile.php
@@ -15,34 +15,93 @@ $userProfile = $statement->fetch(PDO::FETCH_ASSOC);
 if (!empty($_POST)) {
 
     $email = htmlspecialchars($_POST['email']);
+    $password = htmlspecialchars($_POST['password']);
 
-    # check if email has changed
-    if ($email != $userProfile['email']) {
+    if (password_verify($password, $userProfile['password'])) {
 
-        # check if new email is available and is a valid email address
-        if (User::isEmailAvailable($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        # check if email has changed
+        if ($email != $userProfile['email']) {
 
-            # echo !empty($_FILES['image']['size'] == 0) ? 'true' : 'false';
-            # https://stackoverflow.com/questions/14458553/check-if-specific-input-file-is-empty/14458594#14458594
-            # check if a new image has been uploaded - size has to be bigger than 0
+            # check if new email is available and is a valid email address
+            if (User::isEmailAvailable($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+
+                # echo !empty($_FILES['image']['size'] == 0) ? 'true' : 'false';
+                # https://stackoverflow.com/questions/14458553/check-if-specific-input-file-is-empty/14458594#14458594
+                # check if a new image has been uploaded - size has to be bigger than 0
+                if ($_FILES['image']['size'] != 0) {
+
+                    # save new Email + image + description
+
+                    $image = $_FILES['image']['name'];
+                    $description = htmlspecialchars($_POST['description']);
+
+                    # update the database
+                    $updateStatement = $conn->prepare("UPDATE user set description=:newDescription, image=:image, email=:newEmail where email=:sessionEmail");
+                    $updateStatement->bindParam(":newDescription", $description);
+                    $updateStatement->bindParam(":image", $image);
+                    $updateStatement->bindParam(":newEmail", $email);
+                    $updateStatement->bindParam(":sessionEmail", $sessionEmail);
+                    $updateStatement->execute();
+
+                    # change session email value
+                    $_SESSION['email'] = $email;
+
+                    // image file directory
+                    $target = "images/profilePictures/" . $userProfile['id'] . basename($image);
+
+                    // move file
+                    move_uploaded_file($_FILES['image']['tmp_name'], $target);
+
+                    # go back to profile page to view changes
+                    header("location: profile.php");
+
+                } else {
+                    # no new image
+                    # save new email + description
+
+                    $description = htmlspecialchars($_POST['description']);
+                    $email = htmlspecialchars($_POST['email']);
+
+                    $updateStatement = $conn->prepare("UPDATE user set description= :newDescription, email = :newEmail where email = :sessionEmail");
+                    $updateStatement->bindParam(":newEmail", $email);
+                    $updateStatement->bindParam(":newDescription", $description);
+                    $updateStatement->bindParam(":sessionEmail", $sessionEmail);
+                    $updateStatement->execute();
+
+                    # change session email value
+                    $_SESSION['email'] = $email;
+
+                    # go back to profile page to view changes
+                    header("location: profile.php");
+
+
+                }
+
+            } else {
+                if (User::isEmailAvailable($email) == false) {
+                    $error = "This email is not available";
+                } else {
+                    $error = "Please use a valid email address";
+                }
+            }
+
+        } else {
+            # no new email address
+            # check if a new image has been uploaded
+
             if ($_FILES['image']['size'] != 0) {
-
-                # save new Email + image + description
+                # save new image
+                # save description - no need to check, description can be empty
 
                 $image = $_FILES['image']['name'];
                 $description = htmlspecialchars($_POST['description']);
-
-                # update the database
-                $updateStatement = $conn->prepare("UPDATE user set description=:newDescription, image=:image, email=:newEmail where email=:sessionEmail");
+                $updateStatement = $conn->prepare("UPDATE user set description=:newDescription, image=:image where email=:sessionEmail");
                 $updateStatement->bindParam(":newDescription", $description);
-                $updateStatement->bindParam(":image", $image);
-                $updateStatement->bindParam(":newEmail", $email);
                 $updateStatement->bindParam(":sessionEmail", $sessionEmail);
+                $updateStatement->bindParam(":image", $image);
                 $updateStatement->execute();
 
-                # change session email value
-                $_SESSION['email'] = $email;
-
+                # sla images lokaal op
                 // image file directory
                 $target = "images/profilePictures/" . $userProfile['id'] . basename($image);
 
@@ -54,77 +113,23 @@ if (!empty($_POST)) {
 
             } else {
                 # no new image
-                # save new email + description
+                #save description
 
                 $description = htmlspecialchars($_POST['description']);
-                $email = htmlspecialchars($_POST['email']);
-
-                $updateStatement = $conn->prepare("UPDATE user set description= :newDescription, email = :newEmail where email = :sessionEmail");
-                $updateStatement->bindParam(":newEmail", $email);
+                $updateStatement = $conn->prepare("UPDATE user set description= :newDescription where email = :sessionEmail");
                 $updateStatement->bindParam(":newDescription", $description);
                 $updateStatement->bindParam(":sessionEmail", $sessionEmail);
                 $updateStatement->execute();
 
-                # change session email value
-                $_SESSION['email'] = $email;
-
                 # go back to profile page to view changes
                 header("location: profile.php");
-
-
             }
 
-        } else {
-            if (User::isEmailAvailable($email) == false) {
-                $error = "This email is not available";
-            } else {
-                $error = "Please use a valid email address";
-            }
         }
 
     } else {
-        # no new email address
-        # check if a new image has been uploaded
-
-        if ($_FILES['image']['size'] != 0) {
-            # save new image
-            # save description - no need to check, description can be empty
-
-            $image = $_FILES['image']['name'];
-            $description = htmlspecialchars($_POST['description']);
-            $updateStatement = $conn->prepare("UPDATE user set description=:newDescription, image=:image where email=:sessionEmail");
-            $updateStatement->bindParam(":newDescription", $description);
-            $updateStatement->bindParam(":sessionEmail", $sessionEmail);
-            $updateStatement->bindParam(":image", $image);
-            $updateStatement->execute();
-
-            # sla images lokaal op
-            // image file directory
-            $target = "images/profilePictures/" . $userProfile['id'] . basename($image);
-
-            // move file
-            move_uploaded_file($_FILES['image']['tmp_name'], $target);
-
-            # go back to profile page to view changes
-            header("location: profile.php");
-
-        } else {
-            # no new image
-            #save description
-
-            $description = htmlspecialchars($_POST['description']);
-            $updateStatement = $conn->prepare("UPDATE user set description= :newDescription where email = :sessionEmail");
-            $updateStatement->bindParam(":newDescription", $description);
-            $updateStatement->bindParam(":sessionEmail", $sessionEmail);
-            $updateStatement->execute();
-
-            # go back to profile page to view changes
-            header("location: profile.php");
-        }
-
+        $error = "Your password is incorrect. Please try again.";
     }
-
-
 }
 
 
@@ -171,6 +176,10 @@ if (!empty($_POST)) {
                       class="textarea"><?php echo $userProfile['description']; ?></textarea>
         </div>
 
+        <div class="formField">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password">
+        </div>
 
     </fieldset>
 
