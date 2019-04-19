f43818991b08e5895d11c62cf1a5bd50e59e2d59
Arne Ameye <arne.ameye@outlook.com>
2019-04-19 21:13:36 +0200

    added eventlistener and  initial Axios post, response & catch structure

--- js/loadMore.js
+++ js/loadMore.js
@@ -1,1 +1,26 @@
 //AJAX check for email and username via AXIOS
+
+//Select loadMore btn and listen for a click
+const loadMoreBtn = document.querySelector(".loadMoreBtn");
+loadMoreBtn.addEventListener("click", function(){
+
+
+    //make Ajax call via Axios to loadMore.php
+    axios.post('ajax/loadMore.php',{
+        //something: something
+    })
+
+    //response
+    .then(function (response) {
+        
+        //update UI with posts
+                
+    })
+
+    //catch error
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
+});
