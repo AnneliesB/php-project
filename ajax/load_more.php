707219150767d861cfedc53baf69f2ee3ae88f0f
Arne Ameye <arne.ameye@outlook.com>
2019-04-20 12:00:11 +0200

    retrieving last 20 posts results from DB through Ajax

--- ajax/load_more.php
+++ ajax/load_more.php
@@ -1,7 +1,17 @@
 <?php
     require_once("../bootstrap/bootstrap.php");
 
-    $result = ["arne" => "hallo"];
+    //Get connection with DB
+    $conn = Db::getConnection();
+
+    //Get user ID of currently logged in user
+    $user_id = User::getUserId();
+
+    //Get posts from DB and put them in $results
+    $statement = $conn->prepare("select photo.*, user.username from photo INNER JOIN user ON photo.user_id = user.id where user_id IN ( select following_id from followers where user_id = :user_id ) order by id desc limit 20");
+    $statement->bindParam(":user_id", $user_id);
+    $statement->execute();
+    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 
-    echo json_encode($result);
+    echo json_encode($results);
