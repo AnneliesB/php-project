9c8810a545ca00f2da025fb931d10d243c4d0279
Arne Ameye <arne.ameye@outlook.com>
2019-04-21 13:14:50 +0200

    FIX: Loadmore (Ajax) is now working on search results, +cleaning up console log's

--- ajax/load_more.php
+++ ajax/load_more.php
@@ -5,6 +5,8 @@
     $data = json_decode(file_get_contents("php://input"), true);
     //get start point of new posts to load
     $startpoint = $data['shownPosts'];
+    //get searchquery (we'll check if there is one passed later)
+    $searchQuery = $data['searchQuery'];
 
     //Get connection with DB
     $conn = Db::getConnection();
@@ -12,11 +14,23 @@
     //Get user ID of currently logged in user
     $user_id = User::getUserId();
 
-    //Get posts from DB and put them in $results
-    $statement = $conn->prepare("select photo.*, user.username from photo INNER JOIN user ON photo.user_id = user.id where user_id IN ( select following_id from followers where user_id = :user_id ) order by id desc limit $startpoint, 2");
-    $statement->bindParam(":user_id", $user_id);
-    $statement->execute();
-    $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    //check if we are on a search results page (to show only search results and not actual index posts on loading more)
+    if( $searchQuery !== null){
+
+        //we are on a search results page, show more search results!
+        $statement = $conn->prepare("select photo.*, user.username from photo INNER JOIN user ON photo.user_id = user.id where photo.description like '%". $searchQuery ."%' LIMIT $startpoint, 2"); 
+        $statement->execute();   
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    }else{
+
+        //We are not on a search results page => Get posts from DB and put them in $results
+        $statement = $conn->prepare("select photo.*, user.username from photo INNER JOIN user ON photo.user_id = user.id where user_id IN ( select following_id from followers where user_id = :user_id ) order by id desc limit $startpoint, 2");
+        $statement->bindParam(":user_id", $user_id);
+        $statement->execute();
+        $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
 
 
     echo json_encode($results);
