f5eb250cb3a09aa63b10678955a78c9a985146da
Annelies Bellon <annelies_bellon@hotmail.com>
2019-04-28 18:35:04 +0200

    add saveMainColors function which uses the PHPLeague color-extractor library to find the 4 most common colors in an image and saves them to the database
    Using the Palette class, based on the filename and path, the color extractor extracts the 4 most representative colors in the image and saves these in an array. I save each array value in a new variable and insert these into the database.

--- classes/Image.php
+++ classes/Image.php
@@ -1,85 +1,116 @@
-<?php 
-    require_once("bootstrap/bootstrap.php");
+<?php
+require_once("bootstrap/bootstrap.php");
+require 'vendor/autoload.php';
 
-    class Image {
-        public static function getPostId(){ 
-            $conn = Db::getConnection();
-            $statement = $conn->prepare("select id from photo order by id desc limit 1");
-            $statement->execute();
-            $photo = $statement->fetch(PDO::FETCH_ASSOC);
-            $post_id = $photo['id'] + 1;
+use League\ColorExtractor\Color;
+use League\ColorExtractor\ColorExtractor;
+use League\ColorExtractor\Palette;
 
-            // Return current post id
-            return $post_id;
-        }
 
-        public static function checkExtention($image){
-            // Check extention of the image           
-            $allowed =  array('png' ,'jpg', 'jpeg');
-            $ext = pathinfo($image, PATHINFO_EXTENSION);
+class Image
+{
+    public static function getPostId()
+    {
+        $conn = Db::getConnection();
+        $statement = $conn->prepare("select id from photo order by id desc limit 1");
+        $statement->execute();
+        $photo = $statement->fetch(PDO::FETCH_ASSOC);
+        $post_id = $photo['id'] + 1;
 
-            if(in_array($ext, $allowed)) {
-                return true;
-            }
+        // Return current post id
+        return $post_id;
+    }
 
-            else {
-                return false;
-            }
-        }
+    public static function checkExtention($image)
+    {
+        // Check extention of the image           
+        $allowed = array('png', 'jpg', 'jpeg');
+        $ext = pathinfo($image, PATHINFO_EXTENSION);
 
-        public static function saveImageToDb($image, $croppedImage, $description) {
-            $conn = Db::getConnection();
-            $user_id = User::getUserId();
-
-            $statement = $conn->prepare("insert into photo (`description`, `url`, `url_cropped`, `user_id`) VALUES (:description, :image, :croppedImage, :userId)");
-            $statement->bindParam(":description", $description); 
-            $statement->bindParam(":image", $image);
-            $statement->bindParam(":croppedImage", $croppedImage);
-            $statement->bindParam(":userId", $user_id);
-            $result = $statement->execute();        
-        }
-
-        public static function saveImage($image, $imageSaveName){
-            // Image file directory
-            $target = "images/" . basename($image);
-            move_uploaded_file($imageSaveName ,$target);
-        }
-
-        public static function saveCroppedImage($image) {
-            // Image file directory
-            $target = "images/" . basename($image);
-
-            $info = getimagesize($target);
-            $mime = $info['mime'];
-
-                
-            switch($mime) {
-                case 'image/jpeg':
-                    $image_create_func = 'imagecreatefromjpeg';
-                    $image_save_func = 'imagejpeg';
-                    break;
-                    
-                case 'image/png':
-                    $image_create_func = 'imagecreatefrompng';
-                    $image_save_func = 'imagepng';
-                    break;
-            }
-
-            // GET image                
-            $im = $image_create_func($target);
-
-            // CROP image
-            $size = min(imagesx($im), imagesy($im));
-            $im2 = imagecrop($im, ['x' => 0, 'y' => 0, 'width' => $size, 'height' => $size]);
-
-            if ($im2 !== FALSE) {
-                // SAVE cropped image
-                $image_save_func($im2, "images/" . (Image::getPostId() -1)  . 'cropped-' .  $_FILES['image']['name']);
-                imagedestroy($im2);
-            }
-        }
-
-        public static function saveMainColors($imageName){
-
+        if (in_array($ext, $allowed)) {
+            return true;
+        } else {
+            return false;
         }
     }
+
+    public static function saveImageToDb($image, $croppedImage, $description)
+    {
+        $conn = Db::getConnection();
+        $user_id = User::getUserId();
+
+        $statement = $conn->prepare("insert into photo (`description`, `url`, `url_cropped`, `user_id`) VALUES (:description, :image, :croppedImage, :userId)");
+        $statement->bindParam(":description", $description);
+        $statement->bindParam(":image", $image);
+        $statement->bindParam(":croppedImage", $croppedImage);
+        $statement->bindParam(":userId", $user_id);
+        $result = $statement->execute();
+    }
+
+    public static function saveImage($image, $imageSaveName)
+    {
+        // Image file directory
+        $target = "images/" . basename($image);
+        move_uploaded_file($imageSaveName, $target);
+    }
+
+    public static function saveCroppedImage($image)
+    {
+        // Image file directory
+        $target = "images/" . basename($image);
+
+        $info = getimagesize($target);
+        $mime = $info['mime'];
+
+
+        switch ($mime) {
+            case 'image/jpeg':
+                $image_create_func = 'imagecreatefromjpeg';
+                $image_save_func = 'imagejpeg';
+                break;
+
+            case 'image/png':
+                $image_create_func = 'imagecreatefrompng';
+                $image_save_func = 'imagepng';
+                break;
+        }
+
+        // GET image                
+        $im = $image_create_func($target);
+
+        // CROP image
+        $size = min(imagesx($im), imagesy($im));
+        $im2 = imagecrop($im, ['x' => 0, 'y' => 0, 'width' => $size, 'height' => $size]);
+
+        if ($im2 !== FALSE) {
+            // SAVE cropped image
+            $image_save_func($im2, "images/" . (Image::getPostId() - 1) . 'cropped-' . $_FILES['image']['name']);
+            imagedestroy($im2);
+        }
+    }
+
+    public static function saveMainColors($imageName)
+    {
+        $imageBaseName = basename($imageName);
+        $conn = Db::getConnection();
+        $palette = Palette::fromFilename('images/' . $imageName);
+        // an extractor is built from a palette
+        $extractor = new ColorExtractor($palette);
+
+        // it defines an extract method which return the most “representative” colors
+        $colors = $extractor->extract(4);
+        #print_r($colors);
+        $color1 = Color::fromIntToHex($colors[0]);
+        $color2 = Color::fromIntToHex($colors[1]);
+        $color3 = Color::fromIntToHex($colors[2]);
+        $color4 = Color::fromIntToHex($colors[3]);
+
+        $statement = $conn->prepare("UPDATE photo set color1 = :color1, color2 = :color2, color3 = :color3, color4 = :color4 where url = :url");
+        $statement->bindParam(":color1", $color1);
+        $statement->bindParam(":color2", $color2);
+        $statement->bindParam(":color3", $color3);
+        $statement->bindParam(":color4", $color4);
+        $statement->bindParam(":url", $imageBaseName);
+        $statement->execute();
+    }
+}
