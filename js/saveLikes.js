504a3fbf217f8a72386eac9ad0594293fb59e983
Annelies Bellon <annelies_bellon@hotmail.com>
2019-04-29 12:06:10 +0200

    add jquery likes with jquery script link

--- js/saveLikes.js
+++ js/saveLikes.js
@@ -1,36 +1,27 @@
-let knop = document.querySelector("a.like");
-knop.addEventListener("click", function(e){
-    let postId = this.dataset.id;
-    let link = this;
+$("a.like").on("click", function (e) {
+    let postId = $(this).data("id");
+    let link = $(this);
     console.log("test");
 
-    axios.post('ajax/likePost.php',{
-        postId : postId
-
+    $.ajax({
+        method: "POST",
+        url: "ajax/likePost.php",
+        data: {postId: postId},
+        dataType: 'json'
     })
-        .then (function (res){
-            console.log(res);
-            let img = document.querySelector(".postLikeIcon");
-            if (res.data['status'] === "liked") {
-                console.log("we zitten ion de liked");
-
-                let likes = link.nextSibling.innerHTML;
-                console.log(likes + " de likes");
-                link.children.src = "images/liked.svg";
+        .done(function (res) {
+            if (res.status == "liked") {
+                let likes = link.next().html();
+                link.children().attr("src", "images/liked.svg");
                 likes++;
-                link.children.innerHTML=likes;
+                link.next().html(likes);
             } else {
-                console.log("we zitten ion de niet liked");
-                let likes = link.nextSibling.innerHTML;
-                link.children.src = "images/like.svg";
+                let likes = link.next().html();
+                link.children().attr("src", "images/like.svg");
                 likes--;
-                link.nextSibling.innerHTML=likes;
+                link.next().html(likes);
             }
-        })
-        .catch(function (error) {
-            console.log(error);
         });
-
 
     e.preventDefault();
 });
