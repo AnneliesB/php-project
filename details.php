3a46f22e360a6ef5653106c3c5ceb28c00d3fef0
Annelies Bellon <annelies_bellon@hotmail.com>
2019-04-29 14:59:43 +0200

    add script links for jquery and saveLikes so the like feature also works on the details page

--- details.php
+++ details.php
@@ -100,6 +100,7 @@
     <footer>
     
     </footer>
-    
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+    <script src="js/saveLikes.js"></script>
 </body>
 </html>
