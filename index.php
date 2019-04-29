0388874ce24cc059eb141aa86871053ed12782a0
Arne Ameye <arne.ameye@outlook.com>
2019-04-29 22:23:57 +0200

    preparing index and loadmore.js with a link tag for userprofile page on the post template

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ if (!empty($_GET['query'])) {
         <div class="postContainer">
 
             <div class="postTopBar">
-                <div class="postUsername"><?php echo $result['username'] ?></div>
+                <a href="userProfile.php?username=<?php echo $result['username']; ?>"><div class="postUsername"><?php echo $result['username'] ?></div></a>
                 <img class="icon postOptions" src="images/menu.svg" alt="options icon">
 
                 <?php if(User::userHasReported($result['id'], $userId) == true): ?>
