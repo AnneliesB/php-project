13ec78d2a65d75731880e66b8f0233016825cf94
cheaptrills <vincevanalphen@yahoo.com>
2019-05-01 13:15:51 +0200

    added logic to compare useridfrom post with session

--- details.php
+++ details.php
@@ -11,6 +11,11 @@
     $statement->bindParam(":id", $id);
     $statement->execute();
     $post = $statement->fetch(PDO::FETCH_ASSOC);
+    //key value user id vergelijken met session ID
+    if($post["userid"]===$_SESSION["id"]) {
+
+        
+    }
     // GET comments
     $commentStatement = $conn->prepare("select comment.*, user.username from comment inner join user on comment.user_id = user.id where post_id = :postId");
     $commentStatement->bindParam(":postId", $id);
