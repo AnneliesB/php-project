3e273a049053f44997843c38b577d9e81070676f
Annelies Bellon <annelies_bellon@hotmail.com>
2019-05-04 19:47:49 +0200

    add hamburger menu for mobile views

--- nav.incl.php
+++ nav.incl.php
@@ -1,7 +1,19 @@
-<nav class="navbar">
+<nav class="navbar desktop">
     <a href="index.php" class="logo">IMDSTAGRAM</a>
     <a href="index.php">Home</a>
     <a href="profile.php">Profile</a>
     <a href="post.php">New Post +</a>
     <a href="logout.php" class="navbar__logout">Logout</a>
 </nav>
+
+<div class="hamburgerLink mobile"><img src="./images/hamburger-white.svg" alt="image" class="hamburger"></div>
+<nav class="navbar mobile mobileNav">
+    <div>
+        <a href="index.php" class="logo">IMDSTAGRAM</a>
+        <a href="index.php">Home</a>
+        <a href="profile.php">Profile</a>
+        <a href="post.php">New Post +</a>
+        <a href="logout.php" class="navbar__logout">Logout</a>
+    </div>
+</nav>
+
