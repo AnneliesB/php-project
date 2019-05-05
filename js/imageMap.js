eed01ffb1c2bc632241e516fbaac8ecfc73085ed
Arne Ameye <arne.ameye@outlook.com>
2019-05-06 01:03:55 +0200

    added foreach function to loop over a markers array and show actual image markers (with dummy JSON array for now)

--- js/imageMap.js
+++ js/imageMap.js
@@ -9,3 +9,36 @@ var map = new mapboxgl.Map({
     style: "mapbox://styles/mapbox/light-v9"
     
   });
+
+
+//JSON array of images
+var postsJSON = {
+
+    "posts": [
+        {
+            "lat": 50.924695711685304,
+            "lng": 3.624462890625
+        },
+        {
+            "lat": 50.97189158092897,
+            "lng": 3.0158203125
+        },
+        {
+            "lat": 50.85151823530889,
+            "lng": 3.29223632812499
+        }
+    ]
+};
+
+// add markers to map
+postsJSON.posts.forEach(function(marker) { //replace the postsJSON.posts with the acutal posts array
+    // create a DOM element for the marker
+    var el = document.createElement('div');
+    el.className = 'marker';
+    el.style.backgroundImage = 'url(images/0flipper.jpg)';
+     
+    // add marker to map
+    new mapboxgl.Marker(el)
+    .setLngLat([marker.lng , marker.lat])
+    .addTo(map);
+    });
