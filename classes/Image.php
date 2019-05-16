8b0b16ff628dc611d2119a57e6a76335c9c7baca
Annelies Bellon <annelies_bellon@hotmail.com>
2019-05-16 16:30:03 +0200

    add fix for less than 4 colors

--- classes/Image.php
+++ classes/Image.php
@@ -146,18 +146,19 @@ class Image
         # check if we have 4 different colors
 
         #if first two colors are equal, all the other colors will be equal too => 1 color
-        if($colors[0] == $colors[1]){
+
+        if(count($colors) == 1){
             $color1 = ltrim(Color::fromIntToHex($colors[0]), '#');
             $color2 = ltrim(Color::fromIntToHex($colors[0]), '#');
             $color3 = ltrim(Color::fromIntToHex($colors[0]), '#');
             $color4 = ltrim(Color::fromIntToHex($colors[0]), '#');
-        } else if ($colors[1] == $colors[2]){
+        } else if (count($colors) == 2){
             # if color 2 and 3 are equal, color 4 will be the same color => we have 2 colors
             $color1 = ltrim(Color::fromIntToHex($colors[0]), '#');
             $color2 = ltrim(Color::fromIntToHex($colors[1]), '#');
             $color3 = ltrim(Color::fromIntToHex($colors[0]), '#');
             $color4 = ltrim(Color::fromIntToHex($colors[1]), '#');
-        } else if ($colors[2] == $colors[3]){
+        } else if (count($colors) == 3){
             # if color 3 and 4 are equal => we have 3 main colors
             $color1 = ltrim(Color::fromIntToHex($colors[0]), '#');
             $color2 = ltrim(Color::fromIntToHex($colors[1]), '#');
